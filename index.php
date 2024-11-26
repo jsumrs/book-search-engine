48278d49b4ac424ed1aeb1db3873a9020340737e
jasoncodes-ca <github@jasoncodes.ca>
2024-11-25 20:13:57 -0800

    Add basic navbar styling to webpage. Add favicon and logo to page.

--- index.php
+++ index.php
@@ -3,12 +3,13 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="index.css">
+<link rel="icon" type="image/x-icon" href="images/favicon.ico">
 <title>Home</title>
 </head>
 
 <body>
     <nav>
-        <ul class="nav-bar">
+        <ul class="navbar">
             <li><a href="index.php">Home</a></li>
             <li><a href="browse.php">Browse</a></li>
             <li><a href="user_catalog.php">My Library</a></li>
