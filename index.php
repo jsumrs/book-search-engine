b97047d62850d9f2684d42425a3cd39d8692e7f8
jasoncodes-ca <github@jasoncodes.ca>
2024-11-25 16:52:25 -0800

    Init commit.

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="index.css">
+<title>Home</title>
+</head>
+
+<body>
+    <nav>
+        <ul class="nav-bar">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="browse.php">Browse</a></li>
+            <li><a href="user_catalog.php">My Library</a></li>
+        </ul>
+    </nav>
+</body>
+
+</html>
