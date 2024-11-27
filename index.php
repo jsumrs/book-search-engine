b73544039ad82bd46fe659e75c0c4965995965b0
jasoncodes-ca <github@jasoncodes.ca>
2024-11-26 18:52:01 -0800

    Change main stylesheet filename. Create functional navbar.

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
+<?php
+session_start();
+
+if (isset($_SESSION['username'])) {
+    $username = $_SESSION['username'];
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="index.css">
+<link rel="stylesheet" href="main.css">
 <link rel="icon" type="image/x-icon" href="images/favicon.ico">
 <title>Home</title>
 </head>
@@ -12,7 +20,16 @@
         <ul class="navbar">
             <li><a href="index.php">Home</a></li>
             <li><a href="browse.php">Browse</a></li>
-            <li><a href="user_catalog.php">My Library</a></li>
+            <li><a href="user_library.php">My Library</a></li>
+            <?php 
+            if (isset($username)) {
+                echo "<li class='navbar-auth-buttons'><a href='user_library.php'>$username</a></li>";
+            } else {
+                echo "<li class='navbar-auth-buttons'><a href='login.php'>Login</a></li>";
+                echo "<li><a href='register.php'>Register</a></li>";
+            }
+            ?>
+            
         </ul>
     </nav>
 </body>
