873c64de8cce55d5806748d81a251a5ee203be64
jasoncodes-ca <github@jasoncodes.ca>
2024-11-27 17:23:42 -0800

    Add logo and searchbar to navbar. Rename style file to more closely match industry standard.

--- index.php
+++ index.php
@@ -18,14 +18,19 @@ if (isset($_SESSION['username'])) {
 <body>
     <nav>
         <ul class="navbar">
+            <li><a id="navbar-logo-anchor" href="index.php"><img class="navbar-logo" src="images/android-chrome-512x512.png" alt="Company Logo: an open book"></a></li>
             <li><a href="index.php">Home</a></li>
             <li><a href="browse.php">Browse</a></li>
-            <li><a href="user_library.php">My Library</a></li>
+            <li class="navbar-search-box">
+                <form action="process_search.php" method="POST">
+                    <input  type="text" placeholder="Search books..." />
+                </form>
+            </li>
             <?php 
             if (isset($username)) {
-                echo "<li class='navbar-auth-buttons'><a href='user_library.php'>$username</a></li>";
+                echo "<li><a href='user_library.php'>My Library</a></li>";
             } else {
-                echo "<li class='navbar-auth-buttons'><a href='login.php'>Login</a></li>";
+                echo "<li ><a href='login.php'>Login</a></li>";
                 echo "<li><a href='register.php'>Register</a></li>";
             }
             ?>
