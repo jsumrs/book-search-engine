bcf7cd1b84a04da859fbe96d7060537dd3320a58
jasoncodes-ca <github@jasoncodes.ca>
2024-12-03 16:20:47 -0800

    Add search files to repo.
    Search files were not added correctly in aefa0b5ec84067daacb1a06d407ebfe206968d8b.

---
+++ search.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="main.css">
+<link rel="icon" type="image/x-icon" href="images/favicon.ico">
+<title>Home</title>
+</head>
+
+<body>
+
+    <?php include 'navbar.php'; ?>
+    <table id="search-output"></table>
+</body>
+
+</html>
